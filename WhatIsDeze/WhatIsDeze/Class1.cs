2ad5414e18dd50803fe0e84bdf421af94b498bd1
Unknown <rens.dollee@student.hu.nl>
2019-04-23 15:31:59 +0200

    asdf
    asdf

---
+++ WhatIsDeze/WhatIsDeze/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsDeze
+{
+
+    class TicTacToeEngine
+    {
+
+        Dictionary<int, string> dict = new Dictionary<int, string>()
+                                            {
+                                                {1,""},
+                                                {2, ""},
+                                                {3,""},
+                                                {4,""},
+                                                {5,""},
+                                                {6,""},
+                                                {7,""},
+                                                {8,""},
+                                                {9,""}
+                                            };
+
+
+
+        public enum GameStatus { PlayerOPlays, PlayerXPlays, Equal, PlayerOWins, PlayerXWins }
+
+        public GameStatus status = GameStatus.PlayerOPlays;
+
+        public String board()
+        {
+            return dict.ToString();
+        }
+
+        public Boolean ChooseCell(int tni)
+        {
+            bool result = false;
+            if (dict[tni] == "")
+            {
+                if (status == GameStatus.PlayerOPlays) { dict[tni] = "o"; }
+                if (status == GameStatus.PlayerXPlays) { dict[tni] = "x"; }
+
+                result = true;
+            }
+
+            return result;
+        }
+
+        public void reset()
+        {
+            dict = new Dictionary<int, string>()
+                                            {
+                                                {1,""},
+                                                {2, ""},
+                                                {3,""},
+                                                {4,""},
+                                                {5,""},
+                                                {6,""},
+                                                {7,""},
+                                                {8,""},
+                                                {9,""}
+                                            };
+            status = GameStatus.PlayerOPlays;
+
+
+        }
+
+        public String whosTurn()
+        {
+            String result = "";
+            if (status == GameStatus.PlayerOPlays) { result =  "o"; }
+            if (status == GameStatus.PlayerXPlays) { result =  "x"; }
+            if (status == GameStatus.Equal) { result = "equal"; }
+            return result;
+        }
+
+        public Boolean isThereAWinner()
+        {
+            Boolean result = false;
+            String winner = "";
+
+            if(dict[1] == dict[2] && dict[2] == dict[3] && dict[3] != "")
+            {
+                result = true;
+                winner = dict[1];
+
+            }
+            if (dict[4] == dict[5] && dict[5] == dict[6] && dict[6] != "")
+            {
+                result = true;
+                winner = dict[4];
+
+            }
+            if (dict[7] == dict[8] && dict[8] == dict[9] && dict[9] != "")
+            {
+                result = true;
+                winner = dict[7];
+
+            }
+            if (dict[1] == dict[5] && dict[5] == dict[9] && dict[9] != "")
+            {
+                result = true;
+                winner = dict[1];
+
+            }
+            if (dict[1] == dict[4] && dict[4] == dict[7] && dict[7] != "")
+            {
+                result = true;
+                winner = dict[1];
+
+            }
+            if (dict[2] == dict[5] && dict[5] == dict[8] && dict[8] != "")
+            {
+                result = true;
+                winner = dict[2];
+
+            }
+            if (dict[7] == dict[5] && dict[5] == dict[3] && dict[3] != "")
+            {
+                result = true;
+                winner = dict[7];
+            }
+            if (dict[3] == dict[6] && dict[6] == dict[9] && dict[9] != "")
+            {
+                result = true;
+                winner = dict[3];
+            }
+          
+            if (winner == "")
+            {
+                if (dict.ContainsValue(""))
+                {
+                    if (status == GameStatus.PlayerOPlays) { status = GameStatus.PlayerXPlays; }
+                    else if (status == GameStatus.PlayerXPlays) { status = GameStatus.PlayerOPlays; }
+                }
+                else
+                {
+                    winner = "equal";
+                    status = GameStatus.Equal;
+                    result = true;
+                    
+                }
+            }
+            Console.WriteLine(winner);
+
+            return result;
+        }
+    }
+}
---
+++ WhatIsDeze/WhatIsDeze/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhatIsDeze
+{
+    public partial class Form1 : Form
+    {
+        private TicTacToeEngine engine;
+
+        public Form1()
+        {
+            InitializeComponent();
+            engine = new TicTacToeEngine();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            String token = engine.whosTurn();
+            //System.Windows.Forms.MessageBox.Show("Er is geklikt!");
+            if(sender == button1)
+            {
+                if (engine.ChooseCell(1)) { button1.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button2)
+            {
+                if (engine.ChooseCell(2)) { button2.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button3)
+            {
+                if (engine.ChooseCell(3)) { button3.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button4)
+            {
+                 if (engine.ChooseCell(4)) { button4.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button5)
+            {
+                 if (engine.ChooseCell(5)) { button5.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button6)
+            {
+                if (engine.ChooseCell(6)) { button6.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button7)
+            {
+                if (engine.ChooseCell(7)) { button7.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button8)
+            {
+                if (engine.ChooseCell(8)) { button8.Text = token;
+                    checkWinner();
+                }
+            }
+            else if (sender == button9)
+            {
+                if (engine.ChooseCell(9)) { button9.Text = token;
+                    checkWinner();
+                }
+            }
+            
+
+        }
+
+        public void checkWinner()
+        {
+            if (engine.isThereAWinner())
+            {
+                System.Windows.Forms.MessageBox.Show("De winaar is: " + engine.whosTurn());
+                engine.reset();
+                button1.Text = "";
+                button2.Text = "";
+                button3.Text = "";
+                button4.Text = "";
+                button5.Text = "";
+                button6.Text = "";
+                button7.Text = "";
+                button8.Text = "";
+                button9.Text = "";
+            }
+
+        }
+
+     
+    }
+}
